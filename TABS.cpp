ba7335afc5d80e9eb6f9fbb03c2760179af4a63c
QuinnWood <58484953+QuinnWood@users.noreply.github.com>
2020-11-30 20:08:49 -0500

    Update TABS.cpp
    IAN look at it first. Gonna change dialogue and even add a random int to pick from a selection of quotes from eventually so it stays fun for each new playthrough. Skeleton temBuilder func. If any questions just let me know. Will continue to fine tune.

--- TABS.cpp
+++ TABS.cpp
@@ -11,6 +11,11 @@
 #include <cmath>
 using namespace std;
 
+
+   /*      RUN ME FIRST BRO JUST RUN ME FIRST THEN ERASE ME TO FIX ERASE ME AND THE ONE BEFORE MAIN ERASE ME AND THE ONE BEFORE MAIN THERE ARE 3 TO ERASE
+
+
+
 struct Player {
     string playerID;
     string name;
@@ -27,9 +32,9 @@ struct Player {
     int IP = 0;    // IPOuts divided by 3
 };
 
-void openFile(string filename, vector<Player> &vect, vector<Player> &vectYears, unordered_map<string, int> &map) {
+void openFile(string filename, vector<Player>& vect, vector<Player>& vectYears, unordered_map<string, int>& map) {
     ifstream workFile;
-    workFile.open("C:\\Users\\ian\\Desktop\\TABS\\csvs\\"+filename);
+    workFile.open("C:\\Users\\ian\\Desktop\\TABS\\csvs\\" + filename);
     if (workFile.good()) {
         cout << "open" << endl;
     }
@@ -49,7 +54,7 @@ void openFile(string filename, vector<Player> &vect, vector<Player> &vectYears, 
             }
             getline(ss, first, ',');
             getline(ss, last, ',');
-            
+
             Player player;
             player.playerID = id;
             player.name = first + " " + last;
@@ -79,11 +84,11 @@ void openFile(string filename, vector<Player> &vect, vector<Player> &vectYears, 
             getline(ss, atBat, ',');
             getline(ss, garbage, ',');
             getline(ss, hits, ',');
-            
+
             vect.at(map[id]).gamesPlayed = vect.at(map[id]).gamesPlayed + stoi(games);
             vect.at(map[id]).atBats = vect.at(map[id]).atBats + stoi(atBat);
             vect.at(map[id]).hits = vect.at(map[id]).hits + stoi(hits);
-            
+
             Player playerYear;
             playerYear.playerID = id + year + team;
             playerYear.name = vect.at(map[id]).name;
@@ -129,21 +134,237 @@ void openFile(string filename, vector<Player> &vect, vector<Player> &vectYears, 
     workFile.close();
 }
 
+
+
+//BUILDS THE VECTOR FULL OF PLAYERS FOR A TEAM
+
+void teamBuilder(vector<pair<string, vector<Player>>> pos, vector<Player> plyrYear, vector<Player> plyrs, vector<Player> &team, string teamName) {
+    int buildQuality = 0;
+    int plyrStat = 0;
+    int plyrPos = 0;
+    string plyrNm;
+    int yr;
+    int bw;
+    int bwNum;
+    vector<Player> objects;
+    cout << "WELCOME TO THE TEAMBUILDING ASPECT OF TABS ALSO KNOWN AS TOTALLY ACCURATE BASEBALL SIMULATOR, no inspo taken from the other TABS, TOTALLY ACCURATE BATTLE SIMULATOR on steam.......... :]\n";
+    cout << "HOW WOULD YOU LIKE TO PICK THIS TEAM?\n1: GIMME RANDIES\n2: HEH...... YUGI I ALREADY KNOW HOW TO WIN....;) it's advanced search...\n3: BEST/WORST\n";
+    cin >> buildQuality;
+
+    switch (buildQuality) {
+
+    //RANDOMS
+    case 1:
+        cout << "randies it is lol good luck\nAlright next point of contention is do you want the player overall stats or a very specific set of skills that will find you and play for you?\n1:overall\n2: the accuaracy of a dynamax move";
+        cin >> plyrStat;
+
+        switch (plyrStat) {
+
+
+        //OVERALL STATS
+        case 1:
+            for (int i = 0; i < 8; i++) {
+
+                cout << "OverALL k. pick a position\n1:Catcher\n2:LF\n3:CF\n4:RF\n5:TB\n6:FB\n7:SB\n8:HB\n";
+                cin >> plyrPos;
+                //sort function (pos, plyrs, &objects, plyrPos) not overly certain what EXACTLY you all will need for the sort funcs
+                cout << "ight now choose wisely\n";
+                //let them pick and add to the team and then clear the objects vector
+            }
+            break;
+
+
+        //SPECIFIC STATS
+        case 2:
+            for (int i = 0; i < 8; i++) {
+                cout << "details - Sherlock Holmes 221BC\npick a position\n1:Catcher\n2:LF\n3:CF\n4:RF\n5:TB\n6:FB\n7:SB\n8:HB\n";
+                cin >> plyrPos;
+
+
+                if (plyrPos <= 1 || plyrPos > 9) {
+                    cout << "enter a valid input\n";
+                    cin >> plyrPos;
+                    i--;
+                    continue;
+                }
+
+
+                //sort function (pos, plyrs, &objects, plyrPos)
+                cout << "its dangerous to go alone take one of these Joe Schmoes\n";
+                //print out the peeps
+            }
+            break;
+
+
+        default:
+            cout << "lol silly, reenter uwu\n";
+            cin >> plyrStat;
+        }
+        break;
+
+    
+    //HE KNOWS TOO MUCH CUZ HE WANNA SEARCH PEOPLE
+    case 2:
+        cout << "okay, but first do you want to do a specific player with stats from specific years/teams or just overall stats?\n1:specific\n2: overall\n";
+        cin >> plyrStat;
+
+        switch (plyrStat) {
+
+        //SPECIFIC STATS
+        case 1:
+            for (int i = 0; i < 10; i++) {
+                cout << "okay we need a name\n";
+                cin >> plyrNm;
+                cout << "and now for a year that they played\n";
+                cin >> yr;
+                //find the player and add it then clear the objects vector, dunno if we need position played for this
+                //maybe use a generic search func and if not found i.e NULL Player obj then decrement i and do not add anything
+            }
+            break;
+
+        //OVERALL STATS
+        case 2:
+            for (int i = 0; i < 10; i++) {
+                cout << "NAME PLEASE\n";
+                cin >> plyrNm;
+                //search func call
+            }
+            break;
+
+        //INVALID INPUT
+        default:
+            cout << "please enter a valid option\n";
+            cin >> plyrStat;
+        }
+        break;
+
+
+    // BEST/WORST STATS FOR TEAMBUILDING
+    case 3:
+        cout << "Do you want the best or worst?\n1:Best\n2:Worst";
+        cin >> bw;
+
+        switch(bw){
+
+        //BEST
+        case 1:
+            cout << "Gimme the best and be rid of the rest. How do you want your stats, overall or specific?\n1:Overall\n2:specific\n";
+
+            cin >> plyrStat;
+
+            switch (plyrStat) {
+
+                //OVERALL
+            case 1:
+                cout << "Alright how many people would you like to pick from?\n";
+                cin >> bwNum;
+
+                for (int i = 0;i < 10; i++) {
+                    cout << "pick a position to fill.\n1:Catcher\n2:LF\n3:CF\n4:RF\n5:TB\n6:FB\n7:SB\n8:HB\n";
+                    cin >> plyrPos;
+                    //sort func to get the bwNum number of players in plyrPos position
+                }
+                break;
+
+            //SPECIFIC
+            case 2:
+                cout << "Alright how many people would you like to pick from?\n";
+                cin >> bwNum;
+
+                for (int i = 0;i < 10; i++) {
+                    cout << "pick a position to fill.\n1:Catcher\n2:LF\n3:CF\n4:RF\n5:TB\n6:FB\n7:SB\n8:HB\n";
+                    cin >> plyrPos;
+                    //sort func to get the bwNum number of players in plyrPos position
+                }
+                break;
+
+            //INVALID INPUT
+            default:
+                cout << "invalid input\n";
+                cin >> plyrStat;
+            }
+            break;
+
+        //WORST
+        case 2:
+            cout << "Who doesn't love a underdog team? How do you want your stats, overall or specific?\n1:Overall\n2:specific\n";
+
+            cin >> plyrStat;
+
+            switch (plyrStat) {
+
+                //OVERALL
+            case 1:
+                cout << "Alright how many people would you like to pick from?\n";
+                cin >> bwNum;
+
+                for (int i = 0;i < 10; i++) {
+                    cout << "pick a position to fill.\n1:Catcher\n2:LF\n3:CF\n4:RF\n5:TB\n6:FB\n7:SB\n8:HB\n";
+                    cin >> plyrPos;
+                    //sort func to get the bwNum number of players in plyrPos position
+                }
+                break;
+
+                //SPECIFIC
+            case 2:
+                cout << "Alright how many people would you like to pick from?\n";
+                cin >> bwNum;
+
+                for (int i = 0;i < 10; i++) {
+                    cout << "pick a position to fill.\n1:Catcher\n2:LF\n3:CF\n4:RF\n5:TB\n6:FB\n7:SB\n8:HB\n";
+                    cin >> plyrPos;
+                    //sort func to get the bwNum number of players in plyrPos position
+                }
+                break;
+
+            //INVALID INPUT
+            default:
+                cout << "invalid input\n";
+                cin >> plyrStat;
+            }
+        break;
+        }
+    break;
+
+
+    //INVALID INPUT
+    default:
+        cout << "lol silly, reenter uwu\n";
+        cin >> buildQuality;
+    }
+
+}
+
+
+
+
+    */  //ERASE ME TO FIX ERASE ME TO FIX AS WELL AS THE ONE BELOW ME ASKING TO BE ERASED
+
+
+
+
 int main()
 {
-    //INSTANTIATE VECTORS AND VARIABLES
+
+
+
+    /*   ERASE MEEE TO FIX MAIN ERASE ME TO FIX MAIN
     
+
+
+    //INSTANTIATE VECTORS AND VARIABLES
+
     vector<pair<string, vector<Player>>> positions;   //String holds pos and vector of players in that pos
     vector<Player> playersYears;  //Holds all players individual batting years
     vector<Player> players;  //Holds all players overall stats, batting and pitching if they pitched
     unordered_map<string, int> indexMap;  //Holds map of player ids to ints for constant time access to overall statistics
-    
+
     //LOAD FILES AND IMPORT INFORMATION INTO VECTORS
 
     openFile("People.csv", players, playersYears, indexMap);
     openFile("Batting.csv", players, playersYears, indexMap);
     openFile("Pitching.csv", players, playersYears, indexMap);
-    
+
     //MENU SYSTEM
 
     int choice = 0;
@@ -178,6 +399,9 @@ int main()
 
     }
 
+    // teamBuilder(positions, playersYears, players, team1, name1);
+    // teamBuilder(positions, playersYears, players, team2, name2);
+
     /*
     for (int i = 0; i < players.size(); i++) {
         cout << players.at(i).playerID << " - " << players.at(i).name << " - " << "Games Played: " << players.at(i).gamesPlayed;
@@ -186,11 +410,44 @@ int main()
             cout << "Pitching Stats - ERA: " << players.at(i).ERA << endl;
         }
     }
-    
+
     for (int i = 0; i < playersYears.size(); i++) {
         cout << playersYears.at(i).playerID << " - " << playersYears.at(i).name << " - " << "Games Played: " << playersYears.at(i).gamesPlayed;
         cout << " Team: " << playersYears.at(i).team << " Year: " << playersYears.at(i).year;
         cout << " AB: " << playersYears.at(i).atBats << " H: " << playersYears.at(i).hits << " BA: " << playersYears.at(i).battingAvg << endl;
     }
     */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+     cout << "THERE IS NO TABS, GET NAKED\n";
 }
